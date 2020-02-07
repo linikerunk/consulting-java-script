32cd7ea3f52034ddecee252bda8083afc076c4cb
linikerunk <linikerenem@gmail.com>
2020-02-07 02:02:31 -0300

    'iniciando-gulp'

--- ajax/server.js
+++ ajax/server.js
@@ -39,4 +39,15 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    //req.body 
+    // req.query
+    // req.params  /parOuImpar:numeros
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+
+})
+
 app.listen(8080, () => console.log('Executando....'))
