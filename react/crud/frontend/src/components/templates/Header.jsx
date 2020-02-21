7616db6225be274da39400465d14c3dec7448ea2
linikerj <linikerenem@gmail.com>
2020-02-21 15:55:14 -0300

    'add:CRUD-project'

---
+++ react/crud/frontend/src/components/templates/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+export default props =>
+    <header className="header">
+        <h2> One Piece !</h2>
+        <ul>
+            <li>Luffy</li>
+            <li>Zoro</li>
+            <li>Sanji</li>
+            <li>Usopp</li>
+        </ul>
+    </header>
